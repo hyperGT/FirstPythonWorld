9bdaf43055ddcb16cc6d390f4b062dd83bb87090
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-25 19:23:06 -0300

    Tipos primitivos exercicíos

---
+++ Exercices1-25/primitive_types_exercice004.py
@@ -0,0 +1,8 @@
+variable = input('digite algo: ')
+
+print('what is the type?', type(variable))
+print('is a numeric? ', variable.isnumeric())
+print('is a alphanum? ', variable.isalnum())
+print('is a string? ', variable.isalpha())
+print('is a lower case?', variable.islower())
+print('is a upper? ', variable.isupper())
