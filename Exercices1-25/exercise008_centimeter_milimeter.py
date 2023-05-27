3162dd2c350492dcaaac429bb771ac9514c456cf
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-27 18:52:23 -0300

    converter metros em centímetros e milímetros - 008

--- Exercices1-25/exercise008_centimeter_milimeter.py
+++ Exercices1-25/exercise008_centimeter_milimeter.py
@@ -1,8 +1,7 @@
 # convert meters in centimeters and milimeters
 
 m = float(input('Enter lengths in meters to convert: '))
-c = m*100
-mi = m*1000
+c = m * 100  # centimeters
+mi = m * 1000  # milimeters
 
 print(f'lenght: {m} \n centimeter: {c} \n milimeter: {mi}')
-
