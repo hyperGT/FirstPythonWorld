88c4337768061b61bfef67ce4a700a4836806aea
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-27 18:30:03 -0300

    converter metro para centimetros e milimetros - 007

---
+++ Exercices1-25/exercise007_student_average.py
@@ -0,0 +1,9 @@
+# Read the student's grades and return the average
+
+student_name = input('Enter the student name: ')
+grade_1 = float(input('Enter the first grade: '))
+grade_2 = float(input('Enter the second grade: '))
+average = (grade_1 + grade_2) / 2
+
+print('\n' + f'The student {student_name} achieved {average} of average')
+
