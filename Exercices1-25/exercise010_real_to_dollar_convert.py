a8b03fd7c1179983db61e630846d8ec9f2766bb6
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-27 19:04:29 -0300

    Converter real para dólar - 010

--- Exercices1-25/exercise010_real_to_dollar_convert.py
+++ Exercices1-25/exercise010_real_to_dollar_convert.py
@@ -1,11 +1,11 @@
 # Make an algorithm that reads a value in reals and converts it to dollars
 
-valor = float(input('Insira o valor que deseja converter para dólar: '))
-dollar = 4.99
+real = float(input('Insira o valor que deseja converter para dólar: '))
+dollar_value = 4.99
 
-real = valor / dollar
+valor = real / dollar_value
 
-print(' Valor do dólar: US${} \n Valor inserido: R${} \n Conversão: US${:.2f}'.format(dollar, valor, real))
+print(' Valor do dólar: US${} \n Valor inserido: R${} \n Conversão: US${:.2f}'.format(dollar_value, real, valor))
 
 
 
