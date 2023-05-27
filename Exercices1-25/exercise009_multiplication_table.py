f240c42e2dd192140804a17743904dbbcd8a6bee
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-27 18:50:03 -0300

    Tabuada - 009

--- Exercices1-25/exercise009_multiplication_table.py
+++ Exercices1-25/exercise009_multiplication_table.py
@@ -3,10 +3,12 @@
 print('-'*40 + '\n Multiplication table')
 n = int(input(' Enter the number: '))
 
+print('\n' + '-'*10)
 i = 0
 while i <= 10:
     r = n * i
     print(f'{n} x {i} = {r}')
     i += 1
+print('-'*12)
 
 
