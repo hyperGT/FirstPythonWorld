d53928370b3d551ccc5a4c59bf1f657e9201ad87
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-27 18:36:26 -0300

    Tipos primitivos - 004(numeração do exercício)

---
+++ Exercices1-25/primitive_types_exercise004.py
@@ -0,0 +1,26 @@
+variable = input('digite algo: ')
+
+print('what is the type?', type(variable))
+print('is a numeric? ', variable.isnumeric())
+print('is a alphanum? ', variable.isalnum())
+print('is a string? ', variable.isalpha())
+print('is a lower case?', variable.islower())
+print('is a upper? ', variable.isupper())
+
+# Arithim Operators
+
+n1 = 5
+n2 = 2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+p = n1 ** n2
+r = n1 % n2
+
+s = n1 + n2
+sub = n1 - n2
+
+print('\n-' * 5)
+print('\n multiplicação {} \n divisão {:.2f} \n div inteira {} \n potencia {} \n resto {}'.format(m, d, di, p, r),
+      end=' ') 
+print('\n soma {} \n subtração {}'.format(s, sub))
