555752829f80c1b639da6c2ecb4b02f254ce4241
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-27 18:30:30 -0300

    tabuada - 008

---
+++ Exercices1-25/exercise008_centimeter_milimeter.py
@@ -0,0 +1,8 @@
+# convert meters in centimeters and milimeters
+
+m = float(input('Enter lengths in meters to convert: '))
+c = m*100
+mi = m*1000
+
+print(f'lenght: {m} \n centimeter: {c} \n milimeter: {mi}')
+
