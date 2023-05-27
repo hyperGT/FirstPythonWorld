8aa374dab2b32234a9b10ae0c28f37c4533b472d
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-27 18:53:57 -0300

    Clássico receba duas notas do aluno e calcule a média - 007

--- Exercices1-25/exercise007_student_average.py
+++ Exercices1-25/exercise007_student_average.py
@@ -5,5 +5,5 @@ grade_1 = float(input('Enter the first grade: '))
 grade_2 = float(input('Enter the second grade: '))
 average = (grade_1 + grade_2) / 2
 
-print('\n' + f'The student {student_name} achieved {average} of average')
+print('\n' + f'The student {student_name} achieved {average} of average ')
 
