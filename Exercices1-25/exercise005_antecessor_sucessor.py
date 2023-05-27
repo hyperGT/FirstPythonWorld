f3ff88fbc2b96d1316d18c323361223fdbca1853
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-27 18:28:59 -0300

    antecessor e sucessor - 005

---
+++ Exercices1-25/exercise005_antecessor_sucessor.py
@@ -0,0 +1,11 @@
+# Programa que lê um número, e retorna o seu antecessor e o seu sucessor
+
+print('Find the predecessor and the successor of a number ---')
+
+number = int(input('enter the number: '))
+
+predecessor_number = number-1
+successor_number = number+1
+
+print('\n Number: {} \n predecessor: {} \n successor {}'.format(number, predecessor_number, successor_number))
+
